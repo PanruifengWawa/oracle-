fb3675a7ddae3d6b46909c9400357befcd741381
PanruifengWawa <46937922@qq.com>
2016-12-28 20:54:31 +0800

    db

---
+++ dbDesign/sqls/11_drug_user_create_synonym.sql
@@ -0,0 +1,13 @@
+create synonym iadmin for drug_admin.iadmin;
+create synonym imodel for drug_admin.imodel;
+create synonym record for drug_admin.record;
+create synonym daily_records for drug_admin.daily_records;
+create synonym drug for drug_admin.drug;
+create synonym iuser for drug_admin.iuser;
+create synonym buy_record for drug_admin.buy_record;
+
+
+create synonym user_buy_record for drug_admin.user_buy_record;
+
+
+create synonym IDRUG for drug_admin.IDRUG;
